6a76396bb4653c403a26e3d56fb8b9abe82b705a
VinitKarlekar <vinitkarlekar84474@gmail.com>
2025-05-31 22:55:25 +0530

    correction

--- memcache.sh
+++ memcache.sh
@@ -5,4 +5,5 @@ sudo systemctl enable memcached
 sudo systemctl status memcached
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
 sudo systemctl restart memcached
+
 sudo memcached -p 11211 -U 11111 -u memcached -d
